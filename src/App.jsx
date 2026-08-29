8edcf27a3a81be11bed62b1098ed116dc2a24758
Aneek Pal <aneekpal199400@gmail.com>
2026-08-30 01:03:44 +0530

    feat: Lower headline and countdown placement on mobile and PC for clean festoon clearance

--- src/App.jsx
+++ src/App.jsx
@@ -158,8 +158,8 @@ export default function App() {
         onOpenPandalGuide={() => setIsPandalGuideOpen(true)}
       />
 
-      {/* 3. Hero Top Section: Big Bengali Title & Countdown Subtitle */}
-      <section className="relative z-20 w-full flex flex-col items-center justify-start px-4 pt-1 sm:pt-3">
+      {/* 3. Hero Top Section: Big Bengali Title & Countdown Subtitle (Moved down for clean clearance) */}
+      <section className="relative z-20 w-full flex flex-col items-center justify-start px-4 pt-8 sm:pt-12 md:pt-14">
         <CountdownHeader selectedDayKey={selectedDayKey} />
       </section>
 
